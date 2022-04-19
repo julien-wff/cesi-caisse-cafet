2be1f5c6523e7d1da4b17edd7c85c5f7586dac52
Julien <cefadrom1@gmail.com>
2022-04-19 17:22:12 +0200

    feat: CSS helpers

--- web/src/views/NotFound.vue
+++ web/src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="h-screen flex items-center justify-center flex-col gap-4 text-center p-4">
-        <h1 class="sm:text-3xl text-xl font-bold">Cette page n'existe pas</h1>
+    <div class="center flex-col gap-4 text-center p-4">
+        <h1>Cette page n'existe pas</h1>
         <router-link class="link link-primary" to="/">Retourner à l'accueil</router-link>
     </div>
 </template>
