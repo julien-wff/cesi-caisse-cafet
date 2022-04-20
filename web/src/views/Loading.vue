26c44d1e0b018c63e0df6742fe7f869431888797
Julien <cefadrom1@gmail.com>
2022-04-20 22:27:04 +0200

    feat: error message display

--- web/src/views/Loading.vue
+++ web/src/views/Loading.vue
@@ -2,5 +2,12 @@
     <div class="center flex-col p-4 gap-8">
         <h1>Cafet CESI</h1>
         <div class="loader-big"></div>
+        <p v-if="error" class="text-error">{{ error }}</p>
     </div>
 </template>
+
+<script lang="ts" setup>
+defineProps<{
+    error?: string;
+}>();
+</script>
