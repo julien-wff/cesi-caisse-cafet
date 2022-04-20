1f8114fa84966cdc026809ff957c12c9bd96ae4c
Julien <cefadrom1@gmail.com>
2022-04-20 21:38:29 +0200

    feat: GraphQL errors handling

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -1,4 +1,4 @@
-import { GraphQLClient as Client } from 'graphql-request';
+import { ClientError, GraphQLClient as Client } from 'graphql-request';
 
 const endpoint = import.meta.env.VITE_API_ENDPOINT;
 
@@ -8,4 +8,19 @@ export const GraphQLSystemClient = new Client(`${endpoint}/graphql/system`);
 export function updateAuth(token: string) {
     GraphQLClient.setHeader('Authorization', `Bearer ${token}`);
     GraphQLSystemClient.setHeader('Authorization', `Bearer ${token}`);
+}
+
+export function handleGQLError(err: any) {
+    let errorMessage = 'Une erreur est survenue.';
+
+    if (err instanceof ClientError)
+        errorMessage = err.response.errors?.[0].message || errorMessage;
+    else if (err instanceof Error)
+        errorMessage = err.message || errorMessage;
+    else if (typeof err === 'string')
+        errorMessage = err || errorMessage;
+    else
+        errorMessage = err?.toString() || JSON.stringify(err) || errorMessage;
+
+    return new Error(errorMessage);
 }
