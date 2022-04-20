f8f84e72a205a1aaf21b3b044ad83889585df690
Julien <cefadrom1@gmail.com>
2022-04-20 19:06:32 +0200

    feat: store management API

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
 import './app.css';
 
+const pinia = createPinia();
+
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app');
