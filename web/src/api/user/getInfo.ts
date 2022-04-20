80d739c61ab82d325c4cbfe38d1ec673ccae228f
Julien <cefadrom1@gmail.com>
2022-04-20 21:39:05 +0200

    feat: login functionalities

---
+++ web/src/api/user/getInfo.ts
@@ -0,0 +1,20 @@
+import { gql } from 'graphql-request';
+import { GraphQLSystemClient } from '../client';
+
+export function getInfo() {
+    return GraphQLSystemClient.request<InfoResponse>(gql`
+        query {
+            users_me {
+                email
+                first_name
+            }
+        }
+    `);
+}
+
+export interface InfoResponse {
+    users_me: {
+        email: string;
+        first_name: string;
+    };
+}
